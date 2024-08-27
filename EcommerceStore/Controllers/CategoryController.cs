5f336a23b4e8286b0c8c853eb5d58769ab36e771
preet <preet@PREETHS_PC>
2024-08-27 13:11:28 -0400

    Added till Edit button

---
+++ EcommerceStore/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using EcommerceStore.Data;
+using EcommerceStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EcommerceStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+            
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString()) 
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (!ModelState.IsValid) { return View(obj); }
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            
+        }
+
+        public IActionResult Edit( int? id)
+        {
+            if (id == null || id == 0) 
+            {
+                return NotFound();
+            } 
+            Category? category = _db.Categories.Find(id);
+            //Category? category1 = _db.Categories.FirstOrDefault(u=>u.Id==id);
+            //Category? category2 = _db.Categories.Where(u=>u.Id==id).FirstOrDefault();
+            if (category == null) 
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (!ModelState.IsValid) { return View(obj); }
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}
---
+++ EcommerceStore/Models/Category.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required (ErrorMessage ="Category Name is Required")]
+        [MaxLength (50)]
+        [DisplayName(" Category Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Category Name is Required")]
+        [DisplayName("Display Order")]
+        [Range(1,100)]
+        public int DisplayOrder { get; set; }
+    }
+}
